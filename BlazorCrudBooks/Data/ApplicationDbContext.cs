e53d1c20b4958e7f8df727dbd544d48bb1c0047f
José Quiñónez <joserobertoq@outlook.com>
2024-01-24 05:15:11 -0600

    DbContext Added

---
+++ BlazorCrudBooks/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BlazorCrudBooks.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCrudBooks.Data
+{
+	public class ApplicationDbContext : DbContext
+
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+		{
+		}
+
+		//Colocar cada modelo
+		public DbSet<Libro> Libro { get; set; }
+	}
+}
+
---
+++ BlazorCrudBooks/Modelos/Libro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorCrudBooks.Modelos
+{
+    public class Libro
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El título es obligatorio")]
+        public string Tituto { get; set; }
+        [Required(ErrorMessage = "La descripción es obligatoria")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "El autor es obligatorio")]
+        public string Autor { get; set; }
+        [Required(ErrorMessage = "La cantidad de páginas es obligatoria")]
+        public int Paginas { get; set; }
+        [Required(ErrorMessage = "El precio es obligatorio")]
+        public int Precio { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
+
