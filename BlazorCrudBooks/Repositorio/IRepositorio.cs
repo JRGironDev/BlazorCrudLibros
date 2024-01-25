642822e0a13a9a40953658895512e9104dcc9d53
José Quiñónez <joserobertoq@outlook.com>
2024-01-25 10:06:32 -0600

    Implementación de interfaz IRepositorio

---
+++ BlazorCrudBooks/Repositorio/IRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BlazorCrudBooks.Modelos;
+
+namespace BlazorCrudBooks.Repositorio
+{
+	public interface IRepositorio
+	{
+		public Task<List<Libro>> GetLibros();
+
+		public Task<Libro> GetLibroId(int libroId);
+
+		public Task<Libro> CrearLibro(Libro crearLibro);
+
+        public Task<Libro> ActualizarLibro(int libroId, Libro actualizarLibro);
+
+        public Task<Libro> ElimianarLibro(int libroId);
+    }
+}
+
---
+++ BlazorCrudBooks/Repositorio/Repositorio.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using BlazorCrudBooks.Data;
+using BlazorCrudBooks.Modelos;
+
+namespace BlazorCrudBooks.Repositorio
+{
+	public class Repositorio : IRepositorio
+	{
+        private readonly ApplicationDbContext _contexto;
+
+        public Repositorio(ApplicationDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<Libro> ActualizarLibro(int libroId, Libro actualizarLibro)
+        {
+            var libroDesdeBd = await _contexto.Libro.FindAsync(libroId);
+            libroDesdeBd.Tituto = actualizarLibro.Tituto;
+            libroDesdeBd.Descripcion = actualizarLibro.Descripcion;
+            libroDesdeBd.Autor = actualizarLibro.Autor;
+            libroDesdeBd.Paginas = actualizarLibro.Paginas;
+            libroDesdeBd.Precio = actualizarLibro.Precio;
+
+            await _contexto.SaveChangesAsync();
+
+            return libroDesdeBd;
+        }
+
+        public Task<Libro> CrearLibro(Libro crearLibro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Libro> ElimianarLibro(int libroId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Libro> GetLibroId(int libroId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Libro>> GetLibros()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
